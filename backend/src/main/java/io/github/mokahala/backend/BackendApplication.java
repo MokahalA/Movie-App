25083666f3b4b32da1de2dd7af83167d0f3a3fdc
Ahmad El Mokahal <a.mokahal_99@hotmail.com>
2023-07-17 01:31:44 -0400

    Configured MongoDB

--- backend/src/main/java/io/github/mokahala/backend/BackendApplication.java
+++ backend/src/main/java/io/github/mokahala/backend/BackendApplication.java
@@ -2,6 +2,8 @@ package io.github.mokahala.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class BackendApplication {
